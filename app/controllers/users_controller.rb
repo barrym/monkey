96fd51fe74226ac64f30c5e2c795c29c99589f14
Barry Mitchelson <barry.mitchelson@gmail.com>
2009-07-12 22:25:11 +0100

    Added gravatar code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < Clearance::UsersController
 
   def index
+    @users = User.all
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def gravatar_url(email, options = {})
+    options[:rating] ||= nil
+    options[:size] ||= nil
+    options[:default] ||= nil
+
+    grav_url = 'http://www.gravatar.com/avatar.php?'
+    grav_url << "gravatar_id=#{Digest::MD5.new.update(email)}"
+    grav_url << "&rating=#{options[:rating]}" if options[:rating]
+    grav_url << "&size=#{options[:size]}" if options[:size]
+    grav_url << "&default=#{options[:default]}" if options[:default]
+    grav_url
+  end
+
 end
