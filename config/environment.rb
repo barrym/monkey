2ded43faafaaa6c0bf876f2b7f250ae7a6e1016a
Barry Mitchelson <barry.mitchelson@gmail.com>
2009-07-27 22:00:45 +0100

    Froze Haml 2.2.0

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
 
   config.gem "thoughtbot-clearance", :lib => "clearance", :source => "http://gems.github.com", :version => "0.6.9"
   config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com", :version => "1.2.1"
+  config.gem "haml", :version => "2.2.0"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
