d9acdd838a508f8ca2ace3bd361714cfbb9e3f09
Barry Mitchelson <barry.mitchelson@gmail.com>
2009-08-03 15:36:45 +0100

    Hmm, so we don't have a hash column, odd.

--- db/migrate/20090803125953_rename_entity_hash_to_ref.rb
+++
@@ -1,9 +0,0 @@
-class RenameEntityHashToRef < ActiveRecord::Migration
-  def self.up
-    rename_column(:entities, :hash, :ref)
-  end
-
-  def self.down
-    rename_column(:entities, :ref, :hash)
-  end
-end
