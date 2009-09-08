31779d57df6563c77aa1e3e49bff185a06a09c0c
Barry Mitchelson <barry.mitchelson@gmail.com>
2009-09-08 08:37:14 +0100

    Froze Rails 2.3.4

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   config.gem "haml", :version => "2.2.0"
   config.gem "juggernaut"
   config.gem "httparty"
+  config.gem "json", :version => "=1.1.7"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
